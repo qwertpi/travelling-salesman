0f2edb68b02730c11ad686b0512f091b45dadd39
qwertpi <qwertpi@users.noreply.github.com>
2020-09-26 17:46:12 +0100

    Added bounded version of bruteforce to make it more efficent

---
+++ bounded_random_bruteforce.py
@@ -0,0 +1,63 @@
+from copy import copy
+from math import inf
+from random import choice, randint
+##from time import sleep
+import networkx as nx
+
+
+def CalculateLength(graph, nodes):
+    i = 1
+    length = 0
+    start = nodes[0]
+    try:
+        while True:
+            end = nodes[i]
+            length += graph[start][end]["weight"]
+            start = end
+            i += 1
+    except IndexError:
+        return length
+
+graph = nx.Graph()
+#creates a random graph
+#this can be changed easily
+number_of_nodes = 20
+for i in range(0, number_of_nodes):
+    for j in range(i, number_of_nodes):
+        graph.add_edge(i, j, weight=randint(1, 20))
+
+
+nodes = list(graph.nodes())
+best_cycle = []
+best_cycle_len = inf
+
+try:
+    while True:
+        cycle = []
+        #while there is not a full cycle
+        too_long = False
+        while len(cycle) < len(nodes):
+            #picks a random node
+            pick = choice(nodes)
+            #only adds the random node to the cycle if it isn't already in the cycle
+            if pick not in cycle:
+                cycle.append(pick)
+            #there is no point in continuing as the cycle is only going to get longer
+            if CalculateLength(graph, cycle) > best_cycle_len:
+                too_long = True
+                break
+        if not too_long:
+            #closes the cycle by returning to start
+            cycle.append(cycle[0])
+            #calculates total weight of cycle
+            length = CalculateLength(graph, cycle)
+            ##print(f"Trying {cycle} with weight {length}")
+            
+            if best_cycle_len > length:
+                best_cycle = copy(cycle)
+                best_cycle_len = length
+                print(f"Best cycle is {best_cycle} with weight {best_cycle_len}")
+        ##sleep(0.25)
+except KeyboardInterrupt:
+    print(f"Best cycle is {best_cycle} with weight {best_cycle_len}")
+
